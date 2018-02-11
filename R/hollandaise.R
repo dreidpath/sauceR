95ead601693e9eada2876d4fc2abaf8b00472aec
dreidpath <daniel.reidpath@monash.edu>
2018-02-11 09:20:43 +0800

    ...

--- R/hollandaise.R
+++ R/hollandaise.R
@@ -1,17 +1,15 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Build and Reload Package:  'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
+##############################################################
+#                   EXAMPLES OF MODULES                      #
+# This file contains three potential modules:                #
+# #!--Figure1                                                #
+# #!--Figure2                                                #
+# #<<<hello2                                                 #
+#                                                            #
+# The first two modules are marked with the default module   #
+# marker `#!--`.  The last potential module is marked using  #
+# a new marker `#<<<`                                        #
+##############################################################
+
 
 ### This module uses the default module marker
 ### Why change it?
